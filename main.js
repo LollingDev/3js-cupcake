07860a145b01cb2d1c78191a163161bd87832773
Lola Laptop <lollingdev@outlook.com>
2024-01-19 22:35:49 +0000

    Styling changes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0x42d1f5 );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -16,15 +17,15 @@ loader.load( 'Cupcakeblend.glb', function ( gltf ) {
 
     scene.add( gltf.scene );
 
-	const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 0.4 );
-	hemiLight.position.set( 0, 20, 0 );
+	const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 0.9 );
+	hemiLight.position.set( 0, 20, 30 );
 	scene.add( hemiLight );
 
-	const dirLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+	const dirLight = new THREE.DirectionalLight( 0xffffff, 0.9 );
 	dirLight.position.set( - 3, 10, - 10 );
 	scene.add( dirLight );
 
-	camera.position.set( 0, 0, 10 );
+	camera.position.set( 0, 1, 3 );
 
     renderer.render(scene, camera);
 
