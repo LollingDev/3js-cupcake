3435e48bf4135a2ce0803395d8692e41bf859c23
Lola Laptop <lollingdev@outlook.com>
2024-01-19 22:22:00 +0000

    A cupcake is born

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,20 +8,29 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const loader = new GLTFLoader();
 
-camera.position.z = 5;
 
-function animate() {
-	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+loader.load( 'Cupcakeblend.glb', function ( gltf ) {
 
-	renderer.render( scene, camera );
-}
+    scene.add( gltf.scene );
 
-animate();
+	const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 0.4 );
+	hemiLight.position.set( 0, 20, 0 );
+	scene.add( hemiLight );
+
+	const dirLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+	dirLight.position.set( - 3, 10, - 10 );
+	scene.add( dirLight );
+
+	camera.position.set( 0, 0, 10 );
+
+    renderer.render(scene, camera);
+
+}, undefined, function ( error ) {
+
+    console.error( error );
+
+} );
+
