7bac28a31a30c4e0ce2250224c58367f8bd0972b
Lola Laptop <lollingdev@outlook.com>
2024-01-20 23:50:51 +0000

    improved desktop animation

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x42d1f5 );
+scene.background = new THREE.Color( 0x7ec9de );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+window.addEventListener('wheel',animate);
 window.addEventListener('scroll',animate);
-
 const loader = new GLTFLoader();
 
 
