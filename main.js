83b54c59f0351c03f5287bf8206535c5d1f79ab8
Lola Laptop <lollingdev@outlook.com>
2024-01-20 19:15:48 +0000

    cupcake forced to spin

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+window.addEventListener('scroll',animate);
 
 const loader = new GLTFLoader();
 
@@ -15,10 +16,13 @@ const loader = new GLTFLoader();
 
 loader.load( 'Cupcakeblend.glb', function ( gltf ) {
 
-    scene.add( gltf.scene );
+
+	var cupcake = gltf.scene;
+	scene.add(cupcake);
+	
 
 	const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 0.9 );
-	hemiLight.position.set( 0, 20, 30 );
+	hemiLight.position.set( 0, 12, 20 );
 	scene.add( hemiLight );
 
 	const dirLight = new THREE.DirectionalLight( 0xffffff, 0.9 );
@@ -26,6 +30,7 @@ loader.load( 'Cupcakeblend.glb', function ( gltf ) {
 	scene.add( dirLight );
 
 	camera.position.set( 0, 1, 3 );
+	camera.lookAt(0,0,0);
 
     renderer.render(scene, camera);
 
@@ -35,3 +40,18 @@ loader.load( 'Cupcakeblend.glb', function ( gltf ) {
 
 } );
 
+var angle = 0;
+var radius = 3; 
+
+function animate() {
+	
+	console.log(angle)
+
+// Use Math.cos and Math.sin to set camera X and Z values based on angle. 
+camera.position.x = radius * Math.cos( angle );
+camera.position.z = radius * Math.sin( angle );
+camera.lookAt(0,0,0);
+angle += 0.1;
+
+renderer.render(scene, camera);
+}
